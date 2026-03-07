33440055d386413b328d3e6f8fa5d9bd4eccb72c
Eric Aguayo Quintriqueo <e.aguayo@edugalvarino.cl>
2026-03-07 09:56:41 -0300

    Remove Electron configuration and preload script files

--- electron/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge } = require("electron");
-
-// Expose minimal API to renderer
-contextBridge.exposeInMainWorld("electronAPI", {
-  platform: process.platform,
-  isElectron: true,
-});
